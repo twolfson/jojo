44e3629a47bbeff7d6bed6962f79051458e66ebc
Todd Wolfson <todd@twolfson.com>
2012-06-08 20:47:18 -0700

    Getting routing connect-style working

--- app.js
+++ app.js
@@ -6,7 +6,20 @@ app.use(jojo);
 app.listen(8080);
 
 app.get('/abba', function (req, res) {
-  res.send('Not a blog post');
+  res.send([
+    '<script src="https://raw.github.com/twolfson/File-Watcher/master/src/watcher.js"></script>        ',
+    '<script src="https://raw.github.com/twolfson/Resource-Collector/master/src/collector.js"></script>',
+    '<script>                                                                                          ',
+    '    (function () {                                                                                ',
+    '       var watcher = new FileWatcher(),                                                           ',
+    '           resources = ResourceCollector.collect();                                               ',
+    '       watcher.addListener(function () {                                                          ',
+    '         location.reload();                                                                       ',
+    '       });                                                                                        ',
+    '       watcher.watch(resources);                                                                  ',
+    '    }());                                                                                         ',
+    '</script>                                                                                         ',
+  ].join('\n'));
 });
 
 console.log('Server is listening to http://127.0.0.1:8080');
