21db7fd7c5a5e01fc499f4a2d30bddb4dcba20b4
Todd Wolfson <todd@twolfson.com>
2012-06-08 20:13:13 -0700

    Reworking jojo to be more legit

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 var jojo = require('jojo'),
     app = jojo.createServer();
+    
+app.use(jojo);
 
 app.listen(8080);
+
+app.get('/abba', function (req, res) {
+  res.send('Not a blog post');
+});
 
 console.log('Server is listening to http://127.0.0.1:8080');
