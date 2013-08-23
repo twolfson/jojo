0af980052aacffa7aaed0c908eb608760289c819
Todd Wolfson <todd@twolfson.com>
2013-08-22 22:49:37 -0700

    Starting to write out tests

--- test/jojo_test.content.js
+++ test/jojo_test.content.js
@@ -1,6 +1,7 @@
 // Load in dependencies
 var spawn = require('child_process').spawn,
-    assert = require('assert');
+    assert = require('assert'),
+    request = require('request');
 
 // Export doubleshot methods
 module.exports = {
@@ -12,16 +13,39 @@ module.exports = {
   'run via CLI': function () {
 
   },
-  'integrated into a server': function () {
+  // TODO: Relocate logic into `this.cmd`, `this.args`, and helper fn
+  'integrated into a server': {
+    before: function spawnChildProcess (done) {
+      // Start up a child server
+      this.child = spawn('node', ['app.js'], {
+        cwd: __dirname + '/test_files/integrated',
+        stdio: [0, 1, 2]
+      });
 
+      // After it loads, callback
+      setTimeout(done, 200);
+    },
+    after: function killChildProcess (done) {
+      // Kill the child process
+      var child = this.child;
+      child.kill();
+
+      // When it leaves, callback
+      child.on('exit', function (code) {
+        done();
+      });
+    }
   },
   'jojo running with no articles': function () {
 
   },
 
   // Assertions against jojo
-  'serves articles': function () {
-
+  'serves articles': function (done) {
+    request('http://localhost:11550/', function handleResponse (err, res, body) {
+      console.log('ERROR:', err);
+      console.log('BODY:', body);
+    });
   },
   'serves an index page': function () {
 
--- test/test_files/integrated/app.js
+++ test/test_files/integrated/app.js
@@ -1,12 +1,12 @@
 // Require jojo and create a server
-var jojo = require('jojo'),
+var jojo = require('../../../src/jojo'),
     express = jojo.express,
 // jojo.createServer makes an express server and auto-uses jojo
     app = jojo.createServer();
 
-app.listen(8080);
+app.listen(11550);
 
 app.set('view engine', 'ejs');
 app.use(express['static'](process.cwd() + '/public'));
 
-console.log('Server is listening to http://127.0.0.1:8080');
+console.log('Server is listening to http://127.0.0.1:11550');
